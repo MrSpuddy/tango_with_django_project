a73e4ce484bc92d697b1271e7ca983c299d63fae
“Luke <2551710G@student.gla.ac.uk>
2022-02-04 10:46:16 +0000

    Hell

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=128, unique=True)
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self): 
+        return self.name
+
+    # IntegerField = Store Integers
+    views = models.IntegerField(default=0)
+
+    # IntegerField = Store Integers
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    # Foreign Key from Category above, delete associated pages if category is deleted
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    
+    # URLField = Text Field for URLs
+    url = models.URLField()
+    # IntegerField = Store Integers
+    views = models.IntegerField(default=0)
+
+    # Good practice for debugging
+    def __str__(self):
+        return self.title
